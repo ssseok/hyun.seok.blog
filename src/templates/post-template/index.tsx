964892e4b76cf77a09d9761d050c17f311f0046b
Hyun Seok Park <luckseok1@gmail.com>
2025-01-17 13:22:16 +0900

    글10 수정

--- src/templates/post-template/index.tsx
+++ src/templates/post-template/index.tsx
@@ -30,10 +30,10 @@ const PostTemplate: React.FC<PostTemplateProps> = ({ location, data }) => {
       <S.PostContent>
         <div className='markdown' dangerouslySetInnerHTML={{ __html: curPost.html }} />
       </S.PostContent>
-      <S.BuyMeACoffeeWrapper>
+      {/* <S.BuyMeACoffeeWrapper>
         <div>👇 도움이 되셨다면 👇</div>
         <BuyMeACoffee />
-      </S.BuyMeACoffeeWrapper>
+      </S.BuyMeACoffeeWrapper> */}
       <PostNavigator prevPost={prevPost} nextPost={nextPost} />
       <Utterances repo={utterancesRepo} path={curPost.slug} />
     </Layout>
